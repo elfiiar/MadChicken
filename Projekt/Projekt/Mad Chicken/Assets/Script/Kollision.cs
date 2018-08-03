14a7a8c18f3d8ebdbbb0f4792dabed1bf6976a05
Elif Arslan <elif.arslan0412@gmail.com>
2018-08-03 12:52:15 +0200

    Projekt
    wurde modifiziert, sodass nciht mehr der Spieler & die Kamera sich bewegt sondern die Welt.
    Springen wurde richtig implementiert

---
+++ Projekt/Projekt/Mad Chicken/Assets/Script/Kollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Kollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "verloren")
+        {
+            Destroy(gameObject);
+            Variablen.WeltStop = 0;
+        }
+
+        if(other.gameObject.tag == "sammelobjekt")
+        {
+            Destroy(other.gameObject);
+            Score.score += 1;
+        }
+    }
+}
---
+++ Projekt/Projekt/Mad Chicken/Assets/Script/Steuerung.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steuerung : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    //Horizontal velocity
+    public float horizontal = 0;
+
+    //StreckenAnzhal
+    public int streckenAnzahl = 2;
+
+
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Spieler bewegt sich nach vorne
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(horizontal, 0, 0);
+
+         //bewegt sich nach links
+        if(Input.GetKeyDown(KeyCode.A) && (streckenAnzahl>1)){
+            horizontal = -2;
+            //ruft stop() auf
+            StartCoroutine(stop());
+            //damit der Spieler NUR in den 3 Strecken laufen kann
+            streckenAnzahl -= 1;
+        }
+
+        //bewegt sich nach rechts
+        if (Input.GetKeyDown(KeyCode.D) && (streckenAnzahl<=2)){
+            horizontal = 2;
+            //ruft stop() auf
+            StartCoroutine(stop());
+            //damit der Spieler NUR in den 3 Strecken laufen kann
+            streckenAnzahl += 1;
+        }
+       // springen
+        if(Input.GetKeyDown(KeyCode.W)){
+            if (transform.position.y <= 1.5)
+            {
+                rb.AddForce(new Vector3(0, 55, 0), ForceMode.Impulse);
+
+            }
+        }
+
+	}
+    //stoppt nach 0,5s, damit es nicht an den Zaun fährt
+    IEnumerator stop(){
+        yield return new WaitForSeconds(0.5f);
+        horizontal = 0;
+    }
+}
