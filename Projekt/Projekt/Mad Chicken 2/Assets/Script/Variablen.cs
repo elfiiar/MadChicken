d8d4ff4fcf15a1f80f50e049c797e0943b45a41f
Elif Arslan <elif.arslan0412@gmail.com>
2018-08-03 22:53:14 +0200

    Überarbeitet
    kleine Game Over Szene wurde erstellt zur Test-Zwecken

---
+++ Projekt/Projekt/Mad Chicken 2/Assets/Script/Variablen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Variablen : MonoBehaviour {
+    public static float WeltStop = 1;
+
+    //Score
+    public static string status = "";
+    public float warteZeit = 0;
+
+    //Strecke
+    public Transform strecke;
+
+    //Zeit
+    public static float time;
+
+
+	// Use this for initialization
+	void Start () {
+        time += Time.deltaTime;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
