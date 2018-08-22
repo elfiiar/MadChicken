0f5faa25796e2cf0b79ec6eb0567b28919aac547
jugoorette <aminakolasinac@hotmail.de>
2018-08-22 03:04:09 +0200

    Update GameOverManager.cs

--- Projekt/GameOverManager.cs
+++ Projekt/GameOverManager.cs
@@ -1,39 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; 
+using UnityEngine.UI;
 
-public class GameOverManager : MonoBehaviour
+public class GameOver : MonoBehaviour
 {
-    public PlayerHealth playerHealth;       // Reference to the player's health.
-    public float restartDelay = 5f;         // Time to wait before restarting the level
-
-
-    Animator anim;                          // Reference to the animator component.
-    float restartTimer;                     // Timer to count up to restarting the level
-
-
-    void Awake ()
-    {
-        // Set up the reference.
-        anim = GetComponent <Animator> ();
-    }
-
-
-    void Update ()
-    {
-        // If the player has run out of health...
-        if(playerHealth.currentHealth <= 0)
-        {
-            // ... tell the animator the game is over.
-            anim.SetTrigger ("GameOver");
-
-            // .. increment a timer to count up to restarting.
-            restartTimer += Time.deltaTime;
-
-            // .. if it reaches the restart delay...
-            if(restartTimer >= restartDelay)
-            {
-                // .. then reload the currently loaded level.
-                Application.LoadLevel(Application.loadedLevel);
-            }
-        }
-    }
+	public static GameOver Instance;
+	public Text scoreText;
+	
+	private void Awake()
+	{
+		Instance = this;
+	}
+	
+	public void Retry()
+	{
+		Time.timeScale = 1;
+		SceneManagement.LoadScene("Game");
+	}
+	
+	public void BackToMenu()
+	{
+		Time.timeScale = 1;
+		SceneManagement.LoadScene("Menu");
+	}
+	
+	public static void Zeig()
+	{
+		if(!Instance)
+		{
+			Time.timeScale = 0;
+			Instantiate(Resources.Load<GameObject>("GameOver"));
+		}
+	}
 }
