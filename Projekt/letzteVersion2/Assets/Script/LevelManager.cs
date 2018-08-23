3f4c85f30731c031d5414346bed5984b51700d73
Elif Arslan <elif.arslan0412@gmail.com>
2018-08-23 15:31:22 +0200

    LevelManager & Finale Version
    LvlManager wurde überarbeitet., jedoch noch nciht fertig

---
+++ Projekt/letzteVersion2/Assets/Script/LevelManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+    
+    //Idee&Code ist teilweise von https://www.youtube.com/watch?v=8QDwcJIZz9w&t=70s; teilweise selbst
+
+    private GameObject player;
+
+    private const float entfernungVorSpawning = 100.0f;
+    private const int maxSegments = 15;
+
+    private int anzahlAktiverSegmente;
+    private int derzeitigerSpawnZ;
+    private int y1, y2, y3;
+
+    //Lists
+    public List<Segment> availableSegments = new List<Segment>();
+    //[HideInInspector]
+    public List<Segment> segments = new List<Segment>();
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        derzeitigerSpawnZ = 0;
+    }
+
+    private void Start()
+    {
+        //Am Anfang 10 Segmente spawnen
+        for (int i = 0; i < 10; i++)
+        {
+            SpawnSegment();
+        }
+    }
+
+    private void Update()
+    {
+        //spawnt weitere Segmente sobald die Entfernung zum letzten Segment weniger als 100 beträgt
+        if(derzeitigerSpawnZ - player.transform.position.z < entfernungVorSpawning){
+            SpawnSegment();
+        }
+
+        //schaut das nicht mehr als 15 Segmente aktiv sind
+        if(anzahlAktiverSegmente >= maxSegments){
+            segments[anzahlAktiverSegmente - 1].DeSpawn();
+          //  segments.RemoveAt(anzahlAktiverSegmente - 1);
+            anzahlAktiverSegmente--;
+        }
+    }
+
+
+    private void SpawnSegment()
+    {
+        List<Segment> possibleSeg = availableSegments.FindAll(x => x.beginY1 == y1 || x.beginY2 == y2 || x.beginY3 == y3);
+        int id = Random.Range(0, possibleSeg.Count);
+
+        //erstellt ein Gameobject go und Instantiatet ein Segment aus der Liste mit dem Index (id)
+        //Segment s wird dann GetComponent von typ Segment zugewiesen
+        //s wird in Index 0 eingefügt
+        GameObject go = Instantiate(availableSegments[id].gameObject) as GameObject;
+        Segment s = go.GetComponent<Segment>();
+        segments.Insert(0, s);
+
+        y1 = s.endY1;
+        y2 = s.endY2;
+        y3 = s.endY3;
+
+        //s wird LevelMananger als child hinzugefügt
+        s.transform.SetParent(transform);
+        //positioniert in der z Achse * derzeitigerSpawnz
+        s.transform.localPosition = Vector3.forward * derzeitigerSpawnZ;
+
+        //DerzeitigerSpawnZ +=Länge vom Segment 
+        derzeitigerSpawnZ += s.length;
+        //anzahlAktiverSegmente wird addiert
+        anzahlAktiverSegmente++;
+    }
+}  
+
---
+++ Projekt/letzteVersion2/Assets/Script/Steuerung.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+public class Steuerung : MonoBehaviour {
+
+    private CharacterController controller;
+
+    private const float spurBreite = 2.0f;
+    private int spur = 1;
+
+    //Springen
+    private float springKraft = 6.0f;
+    private float gravity = 12.0f;
+    private float verticaleVeloctiy;
+
+    //geschwindigkeit
+    private float geschwindigkeit = 7.0f;
+    private float geschwindigkeitErhöhen = 0.1f;
+   
+
+    private GameObject player;
+    private bool setOnlyOneTime = true;
+
+
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Geschwindigkeit erhöhen
+        int z = (int)player.transform.position.z;
+        if (z % 50 == 0 && setOnlyOneTime)
+        {
+            geschwindigkeit += geschwindigkeitErhöhen;
+           // Debug.Log("geschwindigkeit erhöht");
+            setOnlyOneTime = false;
+        }  
+        else if (z % 50 != 0)
+        {
+            setOnlyOneTime = true;
+        }
+
+
+        //Links
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            spur -= 1;
+            spur = Mathf.Clamp(spur, 0, 2);
+
+        }
+        //Rechts
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            spur += 1;
+            spur = Mathf.Clamp(spur, 0, 2);
+
+        }
+
+        //rechnet aus wo die nächste Position ist
+        Vector3 nächstePosition = Vector3.forward * transform.position.z;
+        if (spur == 0)
+        {
+            nächstePosition += Vector3.left * spurBreite;
+        }
+        else if (spur == 2)
+        {
+            nächstePosition += Vector3.right * spurBreite;
+        }
+
+
+        //Springen
+        if (controller.isGrounded){
+            verticaleVeloctiy = -0.1f;
+
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                verticaleVeloctiy = springKraft;
+            }
+        }
+        verticaleVeloctiy -= (gravity * Time.deltaTime);
+
+        Vector3 moveVector = Vector3.zero;
+        //wo wir sein sollten - wo wir sind.normalized und nehmen x * geschwindigkeit
+        moveVector.x = (nächstePosition - transform.position).normalized.x * geschwindigkeit;
+        moveVector.y = verticaleVeloctiy;
+        moveVector.z = geschwindigkeit;
+
+        //bewege das Huhn
+        controller.Move(moveVector * Time.deltaTime);
+	}
+}
