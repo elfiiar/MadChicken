8a5daaf50f7adf619c66421c0294b920fbf9ef40
Elif Arslan <elif.arslan0412@gmail.com>
2018-08-26 16:41:15 +0200

    Strecken Script
    letzte Version

--- Projekt/strecken.cs
+++ Projekt/strecken.cs
@@ -4,161 +4,43 @@ using UnityEngine;
 
 public class strecken : MonoBehaviour
 {
-    public Transform strecke;
-    public float ScenePositon = 150;
-    public float ScenePositionObj = 25;
-    public float ScenePositionScheune = 10;
-    public float ScenePositionSC = 25;
-    public float ScenePositionEgg = 25;
 
+    //Zäune sind aus dem AssetStore: https://assetstore.unity.com/packages/3d/environments/landscapes/low-poly-nature-pack-52670
+    //FloorMaterial ist aus dem Internet: https://i.pinimg.com/736x/d3/7e/b5/d37eb53f257e3fe43c52829f1a3c2a9b--hand-painted-textures-d-texture.jpg
 
+    //wird im Inspektor per Drag&Drop hinzugefügt
+    public GameObject strecke;
+    //fängt ab der Position 250 an hinzuzufügen
+    public float scenePositon = 250;
+    //Auto Init
+    private GameObject player;
+    //sorgt dafür das es nur einmal pro Framezahl gilt
+    private bool setOnlyOneTime = true;
 
-
-    public Transform eggObj;
-    public Transform steinObj;
-    public Transform blockerObj;
-    public Transform scheuneObj;
-    public Transform scheune2Obj;
-    public Transform schafeObj;
-    public Transform scarecrowobj;
-
-    public float time;
-
-    public int randNum;
     // Use this for initialization
     void Start()
     {
-
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (ScenePositionObj < 500)
+        //erstellt pro Frame, der durch 100 teilbar ist, ein Prefab der Strecke
+        int z = (int)player.transform.position.z;
+        Debug.Log(z);
+        if (z % 100 == 0 && setOnlyOneTime)
         {
-            randNum = Random.Range(0, 15);
-            //______________Eggs_____________
-
-            if (randNum == 1 )
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    var childobj = Instantiate(eggObj, new Vector3(0, 1, ScenePositionEgg+=3), eggObj.rotation);
-                    childobj.transform.parent = transform;
-                }
-            }
-            if (randNum == 2)
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    var childobj = Instantiate(eggObj, new Vector3(2, 1, ScenePositionEgg += 3), eggObj.rotation);
-                    childobj.transform.parent = transform;
-                }
-            }
-            if (randNum == 3 )
-            {
-                for (int i = 0; i < 5; i++)
-                {
-                    var childobj = Instantiate(eggObj, new Vector3(-2, 1, ScenePositionEgg += 3), eggObj.rotation);
-                    childobj.transform.parent = transform;
-                }
-            }
-
-            //______________Blocker_____________
-            if (randNum == 4)
-            {
-                var childobj = Instantiate(blockerObj, new Vector3(0, 0, ScenePositionObj), blockerObj.rotation);
-                childobj.transform.parent = transform;
-
-            }
-            if (randNum == 5)
-            {
-                var childobj = Instantiate(blockerObj, new Vector3(2, 0, ScenePositionObj), blockerObj.rotation);
-                childobj.transform.parent = transform;
-
-            }
-            if (randNum == 6)
-            {
-                var childobj = Instantiate(blockerObj, new Vector3(-2, 0, ScenePositionObj), blockerObj.rotation);
-                childobj.transform.parent = transform;
-
-            }
-
-            //______________Scheune_____________
-
-            if (randNum == 7)
-            {
-                var childobj = Instantiate(scheuneObj, new Vector3(2, 0, ScenePositionScheune), scheuneObj.rotation);
-                var childobj2 = Instantiate(scheune2Obj, new Vector3(-2, 0, ScenePositionScheune), scheune2Obj.rotation);
-                childobj.transform.parent = transform;
-                childobj2.transform.parent = transform;
-
-            }
-
-            //______________Stein_____________
-
-            if (randNum == 8)
-            {
-                var childobj = Instantiate(steinObj, new Vector3(2, 0, ScenePositionObj), steinObj.rotation);
-                childobj.transform.parent = transform;
-
-            }
-            if (randNum == 9)
-            {
-                var childobj = Instantiate(steinObj, new Vector3(-2, 0, ScenePositionObj), steinObj.rotation);
-                childobj.transform.parent = transform;
-
-            }
-            if (randNum == 10)
-            {
-                var childobj = Instantiate(steinObj, new Vector3(0, 0, ScenePositionObj), steinObj.rotation);
-                childobj.transform.parent = transform;
-
-            }
-            //______________vogelscheuche_____________
-
-            if (randNum == 11)
-            {
-                var childobj = Instantiate(scarecrowobj, new Vector3(2, 0, ScenePositionSC), scarecrowobj.rotation);
-                childobj.transform.parent = transform;
-
-            }
-            if (randNum == 12)
-            {
-                var childobj = Instantiate(scarecrowobj, new Vector3(0, 0, ScenePositionSC), scarecrowobj.rotation);
-                childobj.transform.parent = transform;
-
-            }
-            if (randNum == 13)
-            {
-                var childobj = Instantiate(scarecrowobj, new Vector3(-2, 0, ScenePositionSC), scarecrowobj.rotation);
-                childobj.transform.parent = transform;
-
-            }
-        
-
-            ScenePositionObj += 7;
-            ScenePositionScheune += 11;
-            ScenePositionSC += 16;
-            ScenePositionEgg += 10;
-
-        }
-    
-
-
-
-
-        if (ScenePositon < 1000)
-        {
-            
-            var child = Instantiate(strecke, new Vector3(0, 0, ScenePositon), transform.rotation);
+            var child = Instantiate(strecke, new Vector3(0, 0, scenePositon), strecke.transform.rotation);
             child.transform.parent = transform;
-            ScenePositon += 100f;
-
+            scenePositon += 100f;
+            setOnlyOneTime = false;
+           // Debug.Log("strecke erweitert");
+        } 
+        else if(z % 100 != 0)
+        {
+            setOnlyOneTime = true;
         }
-
-
     }
 }
 
