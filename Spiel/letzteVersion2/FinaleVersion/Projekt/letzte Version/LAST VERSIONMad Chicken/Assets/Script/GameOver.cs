140c0def47406628c1beca4e3fb68920e18721c8
Elif Arslan <elif.arslan0412@gmail.com>
2018-08-26 14:30:53 +0200

    Kleine Änderung
    Einigen scripten wurden Kommentare hinzugefügt

---
+++ Spiel/letzteVersion2/FinaleVersion/Projekt/letzte Version/LAST VERSIONMad Chicken/Assets/Script/GameOver.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour
+{
+    public static GameOver Instance;
+    public Text scoreText;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        scoreText.text = "Score: " + Score.score;
+    }
+
+    public void Retry()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void BackToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public static void Zeig()
+    {
+        if (!Instance)
+        {
+            Time.timeScale = 0;
+            Instantiate(Resources.Load<GameObject>("GameOver"));
+        }
+    }
+}
