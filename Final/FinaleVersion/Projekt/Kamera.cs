f783b963eefa3238738eeb29500a193cfd873305
jugoorette <aminakolasinac@hotmail.de>
2018-08-27 15:19:25 +0200

    Add files via upload

---
+++ Final/FinaleVersion/Projekt/Kamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kamera : MonoBehaviour {
+    public Transform focus;
+    public Vector3 abstand = new Vector3(0, 4.0f, -3.0f);
+	// Use this for initialization
+	void Start () {
+        transform.position = focus.position + abstand;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 camPosition = focus.position + abstand;
+        camPosition.x = 0;
+        camPosition.y = 4.0f;
+            transform.position = Vector3.Lerp(transform.position, camPosition, Time.deltaTime);
+	}
+}
